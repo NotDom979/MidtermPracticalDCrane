3e59078f167caedf55232125140cb711c1a678cd
NotDom979 <blkwhte979@gmail.com>
2022-07-15 14:12:32 -0400

    Practical Done -DCrane

--- MidtermPracticalDCrane/MidtermPracticalDCrane.cpp
+++ MidtermPracticalDCrane/MidtermPracticalDCrane.cpp
@@ -2,12 +2,71 @@
 //
 
 #include <iostream>
+#include <string>
+
+std::string Questions(std::string question, std::string a, std::string b, std::string c, std::string d);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int score = 0;
+	std::string input1 = Questions("1. What is the capital of Florida? ", "A. Orlando", "B. Miami", "C. Tallahassee", "D. Winter Park");
+	if (input1 == "C" || input1 == "c")
+	{
+		std::cout << "CORRECT!!\n";
+		score++;
+	}
+	else
+		std::cout << "INCORRECT!\n";
+
+	std::string input2 = Questions("\n2. When is Independence Day in the U.S.A? ", "A. May 5th", "B. July 4th", "C. Janurary 1st", "D. Feburary 14th");
+	if (input2 == "B" || input2 == "b")
+	{
+		std::cout << "CORRECT!!\n";
+		score++;
+	}
+	else
+		std::cout << "INCORRECT!\n";
+
+	std::string input3 = Questions("\n3. What is Full Sail? ", "A. Community College", "B. Middle School", "C. High School", "D. University");
+	if (input3 == "D" || input3 == "d")
+	{
+		std::cout << "CORRECT!!\n";
+		score++;
+	}
+	else
+		std::cout << "INCORRECT!\n";
+
+	std::string input4 = Questions("\n4. 8% of 25 is 2, so what is 25% of 8?", "A. 2", "B. 4", "C. 6", "D. 8");
+	if (input4 == "A" || input4 == "a")
+	{
+		std::cout << "CORRECT!!\n";	
+		score++;
+	}
+	else
+		std::cout << "INCORRECT!\n";
+
+	std::string input5 = Questions("\n5. When do Full Sail students start their Summer Break? ", "A. May 31st", "B. June 5th", "C. August 10th", "D. July 25th");
+	if (input5 == "D" || input5 == "d")
+	{
+		std::cout << "CORRECT!!\n";
+		score++;
+	}
+	else
+		std::cout << "INCORRECT!\n";
+
+	double percent = score / 5.0;
+	std::cout << "\n" << "Final Score: " << score << "/5" << " or " << (percent * 100) << "%" << std::endl;
 }
 
+
+std::string Questions(std::string question, std::string a, std::string b, std::string c, std::string d)
+{
+	std::string input;
+	std::cout << question << std::endl;
+	std::cout << a << "\n" << b << "\n" << c << "\n" << d << std::endl;
+	std::getline(std::cin, input);
+	return input;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
